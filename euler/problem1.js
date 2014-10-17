86f27419a9a8aa3ec2d745d0bd202f3d8a272169
ferose <ferose2@gmail.com>
2014-10-16 23:50:44 -0400

    problem 1

--- euler/problem1.js
+++ euler/problem1.js
@@ -1,5 +1,23 @@
 /**
  * Created by Ferose on 10/16/2014.
  */
+const _ = require("lodash")
 
-var sum = 0;
+function simpleSolution(target) {
+    var numbers = _.range( target )
+    var filtered = _.filter(numbers, function(num) {return num % 3 == 0 || num % 5 == 0})
+    var sum = _.reduce(filtered, function(memo, num) {return memo+num})
+    return sum
+}
+
+function efficientSolution(target) {
+    function sumDivisibleBy(num) {
+        var p = Math.floor(target/num)
+        return num*p*(p+1)/2
+    }
+    return sumDivisibleBy(3)+sumDivisibleBy(5)-sumDivisibleBy(15)
+}
+
+console.log(simpleSolution(1000))
+console.log(efficientSolution(1000))
+console.log(efficientSolution(1000000000))
